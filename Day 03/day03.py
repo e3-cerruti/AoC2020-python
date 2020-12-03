727e043f7886718e4fdadf01d5b7c1f378804891
Stephen Cerruti <scerruti@e3civichigh.com>
2020-12-02 22:16:46 -0800

    Alternate Day 03 Solution Demonstrated an alternate solution that uses a for loop instead of a while loop. This changes the determination of position from recursive to explicit by computing the x for each position based on the iterated y.

--- Day 03/day03.py
+++ Day 03/day03.py
@@ -1,24 +1,25 @@
-import collections
-import re
-
-
 def main():
     with open("input.txt", "r") as f:
         lines = f.read().splitlines()
 
-    slopes = [(1,1), (3, 1), (5, 1), (7, 1), (1, 2)]
+    slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
     width = len(lines[0])
     height = len(lines)
     product = 1
 
     for slope in slopes:
-        position = (0, 0)
         trees = 0
 
-        while position[1] < height:
-            if lines[position[1]][position[0] % width] == '#':
+        # position = (0, 0)
+        # while position[1] < height:
+        #     if lines[position[1]][position[0] % width] == '#':
+        #         trees += 1
+        #     position = tuple(sum(i) for i in zip(position, slope))
+
+        for y in range(0, height, slope[1]):
+            x = (y // slope[1]) * slope[0] % width
+            if lines[y][x] == '#':
                 trees += 1
-            position = tuple(sum(i) for i in zip(position, slope))
 
         print(f'Trees: {trees} on slope {slope}')
         product *= trees
