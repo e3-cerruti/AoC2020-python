c849562a84970e205375934930ed1373e45d86b5
Stephen Cerruti <scerruti@e3civichigh.com>
2020-12-02 21:56:02 -0800

    Solved Day 03

---
+++ Day 03/day03.py
@@ -0,0 +1,30 @@
+import collections
+import re
+
+
+def main():
+    with open("input.txt", "r") as f:
+        lines = f.read().splitlines()
+
+    slopes = [(1,1), (3, 1), (5, 1), (7, 1), (1, 2)]
+    width = len(lines[0])
+    height = len(lines)
+    product = 1
+
+    for slope in slopes:
+        position = (0, 0)
+        trees = 0
+
+        while position[1] < height:
+            if lines[position[1]][position[0] % width] == '#':
+                trees += 1
+            position = tuple(sum(i) for i in zip(position, slope))
+
+        print(f'Trees: {trees} on slope {slope}')
+        product *= trees
+
+    print(f'Product of slopes: {product}')
+
+
+if __name__ == '__main__':
+    main()
